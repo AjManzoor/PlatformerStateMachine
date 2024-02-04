9d7420040254ccd2bfa772a2049c55ff746b67fa
AjManzoor <ajmal_manzoor@hotmail.co.uk>
2024-02-04 11:30:00 +0000

    changed the way blast works

--- Blast/Blast.gd
+++ Blast/Blast.gd
@@ -1,1 +1,13 @@
-extends Area2D
+extends CharacterBody2D
+
+@export var blastAnimation : AnimatedSprite2D
+@export var blastBody : CharacterBody2D
+
+func move():
+	pass
+
+func _ready():
+	blastAnimation.play()
+	move()
+	
+	
